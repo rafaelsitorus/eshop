d307db52cc892a7b337b72e6c9a4610d8ae5cea5
maxwar-coder <maxwar233@gmail.com>
2025-02-14 19:31:35 +0700

    ProductTest and ProductRepo new commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ val seleniumJupiterVersion = "5.0.1"
 val webdrivermanagerVersion = "5.6.3"
 val junitJupiterVersion = "5.9.1"
 
+
 plugins {
     java
     id("org.springframework.boot") version "3.4.2"
@@ -37,27 +38,27 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
     testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
     testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
 }
 
 tasks.register<Test>("unitTest") {
-    description = "Runs unit tests."
+    description = "Runs the unit tests."
     group = "verification"
 
-    filter {
+    filter{
         excludeTestsMatching("*FunctionalTest")
     }
 }
 
 tasks.register<Test>("functionalTest") {
-    description = "Runs functional tests."
+    description = "Runs the functional tests."
     group = "verification"
 
-    filter {
+    filter{
         includeTestsMatching("*FunctionalTest")
     }
 }
--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -1,12 +1,12 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-class ProductTest {
-    Product product;
+public class ProductTest {
 
+    Product product;
     @BeforeEach
     void setUp() {
         this.product = new Product();
@@ -17,16 +17,16 @@ class ProductTest {
 
     @Test
     void testGetProductId() {
-        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.product.getProductId());
+        Assertions.assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.product.getProductId());
     }
 
     @Test
     void testGetProductName() {
-        assertEquals("Sampo Cap Bambang", this.product.getProductName());
+        Assertions.assertEquals("Sampo Cap Bambang", this.product.getProductName());
     }
 
     @Test
     void testGetProductQuantity() {
-        assertEquals(100, this.product.getProductQuantity());
+        Assertions.assertEquals(100, this.product.getProductQuantity());
     }
 }
